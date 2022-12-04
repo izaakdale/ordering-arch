57cd7698dfe8d726ce3e003d433378b0b3f435a4
izaakdale <64863677+izaakdale@users.noreply.github.com>
2022-12-04 03:15:48 +0000

    removed fifo

--- aws/sqs/main.tf
+++ aws/sqs/main.tf
@@ -1,5 +1,7 @@
 resource "aws_sqs_queue" "terraform_queue" {
-  name                        = "tester-queue.fifo"
-  fifo_queue                  = true
-  content_based_deduplication = true
+  name                        = "tester-queue"
+  fifo_queue                  = false
+  # content_based_deduplication = true
+  delay_seconds               = 1
+  message_retention_seconds   = 60
 }
